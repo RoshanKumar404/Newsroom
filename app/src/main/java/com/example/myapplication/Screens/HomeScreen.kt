42a2d3328d3e6f900d1ab05f5ddec56768d24967
Roshankumar404 <roshankuar951@gmail.com>
2025-06-24 15:30:41 +0530

    just made Searchbar part of homescreen

--- app/src/main/java/com/example/myapplication/Screens/HomeScreen.kt
+++ app/src/main/java/com/example/myapplication/Screens/HomeScreen.kt
@@ -5,12 +5,17 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -30,13 +36,14 @@ fun HomeScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(horizontal = 16.dp)
             .background(color = Color.White)
+            .padding(horizontal = 16.dp)
+
     ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp),
+                .padding(top = 16.dp),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
@@ -45,11 +52,54 @@ fun HomeScreen(
                 contentDescription = null,
                 modifier = Modifier.size(23.dp)
             )
-            Column (horizontalAlignment =Alignment.CenterHorizontally) {
-                Image(painter = painterResource(R.drawable.shopping_basket), contentDescription =null, modifier = Modifier.size(23.dp))
-                Text("Basket", fontSize =12.sp, color = Color.Black )
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(
+                    painter = painterResource(R.drawable.shopping_basket),
+                    contentDescription = null,
+                    modifier = Modifier.size(23.dp)
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text("Basket", fontSize = 12.sp, color = Color.Black)
             }
+            //last line of this row
         }
+        //greeting text
+        Text(
+            "Hey , Mr. Roshan  plzz tell us what combination of veggies and fruits are you looking for..",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(bottom = 10.dp)
+        )
+        //Adding searchbar
+        //Spacer(modifier = Modifier.height(10.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .size(60.dp)
+                //.padding(16.dp)
+                .background(
+                    color = Color(0xFFF3F1F1),
+                    shape = RoundedCornerShape(16.dp)
+                ),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+           // verticalAlignment = Alignment.CenterVertically
+
+        ) {
+            Icon(
+                imageVector = Icons.Default.Search, contentDescription = null,
+                modifier = Modifier
+                    .padding(horizontal = 10.dp)
+                    .size(30.dp)
+            )
+            Icon(
+                imageVector = Icons.Default.Edit, contentDescription = null,
+                modifier = Modifier
+                    .padding(horizontal = 10.dp)
+                    .size(30.dp)
+            )
+        }
+
 
         //las line of main column
     }
