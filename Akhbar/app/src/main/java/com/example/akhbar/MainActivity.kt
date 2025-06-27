69e3825ae21ec9059bb76478c0bd552d896b6d9b
Roshankumar404 <roshankuar951@gmail.com>
2025-06-27 21:46:55 +0530

    so made the splash screen

--- Akhbar/app/src/main/java/com/example/akhbar/MainActivity.kt
+++ Akhbar/app/src/main/java/com/example/akhbar/MainActivity.kt
@@ -6,42 +6,43 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.akhbar.ui.theme.AkhbarTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        installSplashScreen()
+//        enableEdgeToEdge()
         setContent {
             AkhbarTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android pop up",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+Text("Hey Roshan", fontSize = 30.sp, fontWeight = FontWeight.Bold)
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AkhbarTheme {
-        Greeting("Android")
-    }
-}
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    AkhbarTheme {
+//        Greeting("Android")
+//    }
+//}
