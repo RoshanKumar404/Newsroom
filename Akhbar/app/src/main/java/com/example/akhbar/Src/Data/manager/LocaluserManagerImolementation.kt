ceb33660818a6c9a8b2457e216d1c0bac54011af
Roshankumar404 <roshankuar951@gmail.com>
2025-07-04 20:45:57 +0530

    just settign up Localuser maanger implementation

---
+++ Akhbar/app/src/main/java/com/example/akhbar/Src/Data/manager/LocaluserManagerImolementation.kt
@@ -0,0 +1,20 @@
+package com.example.akhbar.Src.Data.manager
+//
+//import android.content.Context
+//import com.example.akhbar.Src.Domain.manager.LocalUserManager
+//import kotlinx.coroutines.flow.Flow
+//import java.util.prefs.Preferences
+//
+//class LocaluserManagerImolementation (
+//    private val  context:Context
+//    ):LocalUserManager{
+//    override suspend fun saveAppEntry() {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun readAppEntry(): Flow<Boolean> {
+//        TODO("Not yet implemented")
+//    }
+//
+//}
+//private val  context.datastore:DataStore<Preferences>by preferencDataStore(name =)
